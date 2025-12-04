af04bab46bebbcca94fa0eb68e3cb880ce82c54c
Lucas Oliveira <lucas.santos661@hotmail.com>
2025-12-04 07:49:28 -0300

    erro backend nuvem

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const urlBase = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
+if (import.meta.env.PROD && !import.meta.env.VITE_API_URL) {
+  console.warn('Env VITE_API_URL não está definido');
+}
+
 const api = axios.create({
   baseURL: urlBase,
 });
@@ -18,11 +22,11 @@ api.interceptors.response.use(
   (error) => {
     console.log("🚨 INTERCEPTOR API RECEBEU ERRO:", error.response);
     if (error.response && error.response.status === 403) {
-        console.log("⛔ Detectado 403 - Iniciando protocolo de logout...");
-        const msg = error.response.data.error;
-        alert(`Sua conta está bloqueada: ${msg}`);
-        localStorage.removeItem('barber_token');
-        window.location.href = '/'; 
+      console.log("⛔ Detectado 403 - Iniciando protocolo de logout...");
+      const msg = error.response.data.error;
+      alert(`Sua conta está bloqueada: ${msg}`);
+      localStorage.removeItem('barber_token');
+      window.location.href = '/';
     }
     return Promise.reject(error);
   }
